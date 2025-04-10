9db43cd3d31e0ae65cecad4aaa3443c7a1200676
Michael Panzlaff <michael.panzlaff@fau.de>
2025-04-10 11:38:33 +0200

    fix #8

--- internal/aggregator/medianAggregator.go
+++ internal/aggregator/medianAggregator.go
@@ -17,17 +17,21 @@ type MedianAggregatorConfig struct {
 	DeviceType        string `json:"deviceType"`
 	PowerMetric       string `json:"powerMetric"`
 	PerformanceMetric string `json:"performanceMetric"`
-	WindowSize        int    `json:"windowSize"`
+}
+
+type MedianDeviceState struct {
+	powerSamples       []float64
+	performanceSamples []float64
+	powerReset         bool
+	performanceReset   bool
 }
 
 type MedianAggregator struct {
-	// map[hostname]map[deviceId][]sample
-	powerSamples       map[string]map[string][]float64
-	performanceSamples map[string]map[string][]float64
+	// map[hostname]map[deviceId]MedianDeviceState
+	devices            map[string]map[string]*MedianDeviceState
 	powerMetric        string
 	performanceMetric  string
 	deviceType         string
-	windowSize         int
 }
 
 func NewMedianAggregator(rawConfig json.RawMessage) (*MedianAggregator, error) {
@@ -45,17 +49,10 @@ func NewMedianAggregator(rawConfig json.RawMessage) (*MedianAggregator, error) {
 		return nil, err
 	}
 
-	if config.WindowSize <= 0 {
-		cclog.Errorf("MedianAggregator: Window size must be >= 1: Adjusting to 1")
-		config.WindowSize = 1
-	}
-
 	ag.powerMetric = config.PowerMetric
 	ag.performanceMetric = config.PerformanceMetric
-	ag.powerSamples = make(map[string]map[string][]float64)
-	ag.performanceSamples = make(map[string]map[string][]float64)
+	ag.devices = make(map[string]map[string]*MedianDeviceState)
 	ag.deviceType = config.DeviceType
-	ag.windowSize = config.WindowSize
 
 	return ag, nil
 }
@@ -66,46 +63,41 @@ func (a *MedianAggregator) AggregateMetric(m lp.CCMessage) {
 		return
 	}
 
-	if m.Name() == a.powerMetric {
-		// create host specific map if it does not exist yet
-		deviceIdToPowerSamples, ok := a.powerSamples[hostname]
+
+	var deviceState *MedianDeviceState
+	if m.Name() == a.powerMetric || m.Name() == a.performanceMetric {
+		devicesOfHostState, ok := a.devices[hostname]
 		if !ok {
-			deviceIdToPowerSamples = make(map[string][]float64)
-			a.powerSamples[hostname] = deviceIdToPowerSamples
+			devicesOfHostState = make(map[string]*MedianDeviceState)
+			a.devices[hostname] = devicesOfHostState
 		}
 
-		// create device specific window
-		if _, ok := deviceIdToPowerSamples[deviceId]; !ok {
-			deviceIdToPowerSamples[deviceId] = make([]float64, 0)
+		deviceState, ok = devicesOfHostState[deviceId]
+		if !ok {
+			deviceState = &MedianDeviceState{
+				powerSamples: make([]float64, 0),
+				performanceSamples: make([]float64, 0),
+			}
+			devicesOfHostState[deviceId] = deviceState
+		}
+	}
+
+	if m.Name() == a.powerMetric {
+		// insert measurement into history, discard old values if aggregation was performed since last metric
+		if deviceState.powerReset {
+			deviceState.powerSamples = make([]float64, 0)
+			deviceState.powerReset = false
 		}
 
-		// insert measurement into history, discard old values if window is to big
-		l := len(deviceIdToPowerSamples[deviceId])
-		if l >= a.windowSize {
-			deviceIdToPowerSamples[deviceId] = append(deviceIdToPowerSamples[deviceId][:l-1], value)
-		} else {
-			deviceIdToPowerSamples[deviceId] = append(deviceIdToPowerSamples[deviceId], value)
-		}
+		deviceState.powerSamples = append(deviceState.powerSamples, value)
 	} else if m.Name() == a.performanceMetric {
 		// create host specific map if it does not exist yet
-		deviceIdToPerformanceSamples, ok := a.performanceSamples[hostname]
-		if !ok {
-			deviceIdToPerformanceSamples = make(map[string][]float64)
-			a.performanceSamples[hostname] = deviceIdToPerformanceSamples
+		if deviceState.performanceReset {
+			deviceState.performanceSamples = make([]float64, 0)
+			deviceState.performanceReset = false
 		}
 
-		// create device specific window
-		if _, ok := deviceIdToPerformanceSamples[deviceId]; !ok {
-			deviceIdToPerformanceSamples[deviceId] = make([]float64, 0)
-		}
-
-		// insert measurement into history, discard old values if window is to big
-		l := len(deviceIdToPerformanceSamples[deviceId])
-		if l >= a.windowSize {
-			deviceIdToPerformanceSamples[deviceId] = append(deviceIdToPerformanceSamples[deviceId][:l-1], value)
-		} else {
-			deviceIdToPerformanceSamples[deviceId] = append(deviceIdToPerformanceSamples[deviceId], value)
-		}
+		deviceState.performanceSamples = append(deviceState.performanceSamples, value)
 	}
 }
 
@@ -113,28 +105,23 @@ func (a *MedianAggregator) GetPdpPerTarget() map[Target]float64 {
 	// calculate power delay product (PDP) per host and per device
 	pdp := make(map[string]map[string]float64)
 
-	for hostname, deviceIdToPowerSamples := range a.powerSamples {
-		deviceIdToPerformanceSamples, ok := a.performanceSamples[hostname]
-		if !ok {
-			// Initially it may happen that we haven't received a performance sample yet.
-			// Ignore, since it will hopefully arrive until the next iteration.
-			continue
-		}
-
+	for hostname, deviceIdToState := range a.devices {
 		pdp[hostname] = make(map[string]float64)
 
-		for deviceId, powerSampleWindow := range deviceIdToPowerSamples {
-			performanceSampleWindow, ok := deviceIdToPerformanceSamples[deviceId]
-			if !ok {
-				// same here
+		for deviceId, deviceState := range deviceIdToState {
+			if len(deviceState.powerSamples) == 0 && len(deviceState.performanceSamples) == 0 {
+				// Initially it may happen that we haven't received a performance sample yet.
+				// Ignore, since it will hopefully arrive until the next iteration.
 				continue
 			}
 
-			// When the window is created, it is always filled with the first value,
-			// thus the window is never empty and Median won't fail.
-			powerMedian, _ := util.Median(powerSampleWindow)
-			performanceMedian, _ := util.Median(performanceSampleWindow)
+			// The length of both sample arrays is always > 0, thus Median can't fail.
+			powerMedian, _ := util.Median(deviceState.powerSamples)
+			performanceMedian, _ := util.Median(deviceState.performanceSamples)
 			pdp[hostname][deviceId] = powerMedian / performanceMedian
+
+			deviceState.powerReset = true
+			deviceState.performanceReset = true
 		}
 	}
 
