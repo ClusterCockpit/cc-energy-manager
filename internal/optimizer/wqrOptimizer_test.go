f8d36f333e380ec870b4ab86de104484c090c4a7
Michael Panzlaff <michael.panzlaff@fau.de>
2025-04-29 10:59:31 +0200

    optimizer: add wqr convergence test

--- internal/optimizer/wqrOptimizer_test.go
+++ internal/optimizer/wqrOptimizer_test.go
@@ -7,6 +7,12 @@ package optimizer
 import (
 	"encoding/json"
 	//"fmt"
+	"bufio"
+	"os"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
 	"testing"
 
 	cclog "github.com/ClusterCockpit/cc-lib/ccLogger"
@@ -24,7 +30,7 @@ func TestWQRInit(t *testing.T) {
 	cclog.Init("debug", false)
 	_, err := NewWQROptimizer(json.RawMessage(testconfig))
 	if err != nil {
-		t.Fatalf("failed to init WQROptimizer: %v", err.Error())
+		t.Fatalf("failed to init WQROptimizer: %v", err)
 	}
 }
 
@@ -32,7 +38,7 @@ func TestWQRInsertSample(t *testing.T) {
 	cclog.Init("debug", false)
 	o, err := NewWQROptimizer(json.RawMessage(testconfig))
 	if err != nil {
-		t.Fatalf("failed to init WQROptimizer: %v", err.Error())
+		t.Fatalf("failed to init WQROptimizer: %v", err)
 	}
 
 	o.InsertSample(300.0, 0.0)
@@ -58,7 +64,7 @@ func TestWQRCleanupOldSamples(t *testing.T) {
 	cclog.Init("debug", false)
 	o, err := NewWQROptimizer(json.RawMessage(testconfig))
 	if err != nil {
-		t.Fatalf("failed to init WQROptimizer: %v", err.Error())
+		t.Fatalf("failed to init WQROptimizer: %v", err)
 	}
 
 	totalSamples := 50
@@ -82,5 +88,119 @@ func TestWQRCleanupOldSamples(t *testing.T) {
 	// left in the end.
 	if len(o.samples) != 44 {
 		t.Fatalf("Deleted the wrong number of samples: remaining samples %d, but expected 44", len(o.samples))
+	}
+}
+
+func TestWQROptimize(t *testing.T) {
+	samples := LoadSamples(t, "testdata/firestarter.bergamo1", 0)
+
+	o, err := NewWQROptimizer(json.RawMessage(testconfig))
+	if err != nil {
+		t.Fatalf("failed to init WQROptimizer: %v", err)
+	}
+
+	newLimit, warm1done := o.Start(42.0)
+	//fmt.Printf("[ W1] newLimit=%f\n", newLimit)
+	newLimit, warm2done := o.Start(ProbeSample(t, samples, newLimit))
+	//fmt.Printf("[ W2] newLimit=%f\n", newLimit)
+	newLimit, warm3done := o.Start(ProbeSample(t, samples, newLimit))
+	//fmt.Printf("[ W3] newLimit=%f\n", newLimit)
+	if warm1done || warm2done || !warm3done {
+		t.Fatalf("Warmup didn't end in expected 3 steps")
+	}
+
+	for i := 0; i < 30; i++ {
+		newLimit = o.Update(ProbeSample(t, samples, newLimit))
+		//fmt.Printf("[%3d] newLimit=%f\n", i, newLimit)
+	}
+
+	if newLimit < 250 || newLimit > 280 {
+		t.Fatal("WQR optimizer did not converge correctly")
+	}
+
+	//fmt.Printf("========== CHANGE ==========")
+	//samples = LoadSamples(t, "testdata/firestarter.genoa1", 0)
+
+	//for i := 0; i < 100 ; i++ {
+	//	newLimit = o.Update(ProbeSample(t, samples, newLimit))
+	//	fmt.Printf("[%3d] newLimit=%f\n", i, newLimit)
+	//}
+}
+
+func LoadSamples(t *testing.T, path string, socket int) []SamplePoint {
+	file, err := os.Open(path)
+	if err != nil {
+		t.Fatalf("Unable to open test sample file: %v", err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	result := make([]SamplePoint, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		elements := strings.Split(line, ";")
+		if len(elements) != 6 {
+			continue
+		}
+		if elements[0] == "SOCKET" {
+			continue
+		}
+		socketTest, err := strconv.Atoi(strings.TrimSpace(elements[0]))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if socket != socketTest {
+			continue
+		}
+		powerLimit, err := strconv.ParseFloat(strings.TrimSpace(elements[1]), 64)
+		if err != nil {
+			t.Fatal(err)
+		}
+		edp, err := strconv.ParseFloat(strings.TrimSpace(elements[4]), 64)
+		if err != nil {
+			t.Fatal(err)
+		}
+		result = append(result, SamplePoint{PowerLimit: powerLimit, EDP: edp})
+	}
+	if err := scanner.Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].PowerLimit < result[j].PowerLimit
+	})
+
+	return result
+}
+
+func ProbeSample(t *testing.T, samples []SamplePoint, powerLimit float64) float64 {
+	if len(samples) == 0 {
+		return 0.0
+	}
+	cmpFunc := func(s SamplePoint, t float64) int {
+		if s.PowerLimit < t {
+			return -1
+		}
+		if s.PowerLimit > t {
+			return 1
+		}
+		return 0
+	}
+
+	// Find nearest sample
+	pos, _ := slices.BinarySearchFunc(samples, powerLimit, cmpFunc)
+	if pos >= len(samples) {
+		return samples[pos-1].EDP
+	} else if pos > 0 {
+		l := samples[pos-1]
+		r := samples[pos]
+		if powerLimit-l.PowerLimit < r.PowerLimit-powerLimit {
+			return l.EDP
+		} else {
+			return r.EDP
+		}
+	} else {
+		return samples[pos].EDP
 	}
 }
