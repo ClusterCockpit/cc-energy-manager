734b66366c986787372c99e98dfd9e1df5e0c81d
Michael Panzlaff <michael.panzlaff@fau.de>
2025-08-04 16:53:58 +0200

    Clarify power budget vs. power limit

--- internal/clustermanager/clusterManager.go
+++ internal/clustermanager/clusterManager.go
@@ -547,7 +547,7 @@ func (cm *clusterManager) UpdateJobPowerLimits(cluster string) error {
 	for jobId, job := range cm.clusters[cluster].jobIdToJob {
 		for deviceId, jobManager := range job.deviceTypeToJobMgr {
 			power := jobIdToDeviceWeight[jobId][deviceId] * powerFactor
-			cclog.Debugf("Setting power for job %v to %v", jobId, power)
+			cclog.Debugf("Setting power budget for job %v to %v", jobId, power)
 			jobManager.PowerBudgetSet(power)
 		}
 	}
